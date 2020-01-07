84a3f46127cb58c1d2961822bd07f6bf71b3c195
leonid1793 <leonid0@hotmail.es>
2020-01-07 13:41:39 -0500

    Agregar archivos de proyecto.

---
+++ Lab14/Lab14/ITextToSpeech.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lab14
+{
+    public interface ITextToSpeech
+    {
+        void Speak(string text);
+    }
+
+}
---
+++ Lab14/Lab14/TodoItem.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lab14
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public int DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNa { get; set; }
+        public bool Done { get; set; }
+    }
+
+}
